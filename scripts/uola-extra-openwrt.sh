82c442aeda30bb3b79c4095ce4b33d486b3d31bf
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-29 05:47:25 +0800

    Update uola-extra-openwrt.sh

--- scripts/uola-extra-openwrt.sh
+++ scripts/uola-extra-openwrt.sh
@@ -7,8 +7,8 @@ sed -i 's/OpenWrt/danxiaonuo/g' package/base-files/files/bin/config_generate
 sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 # 增加IPV6
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/etc/99-ipv6 > package/base-files/files/etc/hotplug.d/99-ipv6
-sed -i '/exit 0/i\mv /etc/hotplug.d/99-ipv6 /etc/hotplug.d/iface/99-ipv6' package/lean/default-settings/files/zzz-default-settings
-sed -i '/99-ipv6/a\chmod u+x /etc/hotplug.d/iface/99-ipv6' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit 0/i\mv /etc/hotplug.d/99-ipv6 /etc/hotplug.d/iface/99-ipv6' package/xiaonuo/default-settings/files/zzz-default-settings
+sed -i '/99-ipv6/a\chmod u+x /etc/hotplug.d/iface/99-ipv6' package/xiaonuo/default-settings/files/zzz-default-settings
 # 增加 SSID 2.5G
 sed -i '/channel="11"/a\\t\tssid="danxiaonuo-2HZ"' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 增加 SSID 5.0G
@@ -24,7 +24,7 @@ sed -i '/set wireless.default_radio${devidx}.encryption=sae-mixed/a\\t\t\tset wi
 # 修改smartdns配置
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/smartdns/newsmartdns > package/base-files/files/etc/newsmartdns
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/smartdns/smartdns.conf > package/base-files/files/etc/smartdns.conf
-sed -i '/exit 0/i\# 修改smartdns配置\nmv /etc/newsmartdns /etc/config/smartdns\nmv /etc/smartdns.conf /var/etc/smartdns/smartdns.conf' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit 0/i\# 修改smartdns配置\nmv /etc/newsmartdns /etc/config/smartdns\nmv /etc/smartdns.conf /var/etc/smartdns/smartdns.conf' package/xiaonuo/default-settings/files/zzz-default-settings
 # 修改系统欢迎词
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/etc/banner > package/base-files/files/etc/banner
 # 修改系统内核参数
