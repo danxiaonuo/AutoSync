c3881949b455d6f5ab015c556f49f15a0f293a8a
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-30 01:17:01 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -6,8 +6,9 @@ svn co https://github.com/danxiaonuo/AutoSync/trunk/server/smartdns_config xiaon
 find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/smartdns_config xiaonuo/smartdns_config
 find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
-git clone https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
+rm -rf xiaonuo/default-settings xiaonuo/luci-theme-argon
+git clone -b master https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
 find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
-git clone https://github.com/jerrykuku/luci-theme-argon.git xiaonuo/luci-theme-argon
+git clone -b  master https://github.com/jerrykuku/luci-theme-argon.git xiaonuo/luci-theme-argon
 find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
 find /. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
