2e6b18c3507d1797c6277c7206b4cd1505ab8c38
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-30 02:00:52 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -4,11 +4,10 @@ svn co https://github.com/danxiaonuo/AutoSync/trunk/server/net6 xiaonuo/net6
 find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/smartdns_config xiaonuo/smartdns_config
 find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
-svn co https://github.com/danxiaonuo/AutoSync/trunk/server/smartdns_config xiaonuo/smartdns_config
-find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
+find . -type d -iname '.svn' | xargs rm -rf
 rm -rf xiaonuo/default-settings
 rm -rf xiaonuo/luci-theme-argon
-git clone https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
+git clone https://github.com/danxiaonuo/default-settings xiaonuo/default-settings/
 find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
-git clone https://github.com/jerrykuku/luci-theme-argon.git xiaonuo/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git xiaonuo/luci-theme-argon/
 find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
