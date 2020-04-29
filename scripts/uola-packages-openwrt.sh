dbdb6d0c1c8819b06978e4bccaa4f7722b24b8cd
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-30 01:34:19 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -1,13 +1,13 @@
 svn co https://github.com/danxiaonuo/uola/trunk/package/xiaonuo xiaonuo
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf  {} \; >& /dev/null
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/net6 xiaonuo/net6
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf {} \; >& /dev/null
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/smartdns_config xiaonuo/smartdns_config
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf {} \; >& /dev/null
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/smartdns_config xiaonuo/smartdns_config
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
-rm -rf xiaonuo/default-settings xiaonuo/luci-theme-argon
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf {} \; >& /dev/null
+rm -rf xiaonuo/default-settings && rm -rf xiaonuo/luci-theme-argon
 git clone -b master https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf {} \; >& /dev/null
 git clone -b  master https://github.com/jerrykuku/luci-theme-argon.git xiaonuo/luci-theme-argon
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf {} \; >& /dev/null
