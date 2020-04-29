802688654e94a5f2baef0cdbb353b43c6b98870c
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-30 02:22:24 +0800

    Update uola-extra-openwrt.sh

--- scripts/uola-extra-openwrt.sh
+++ scripts/uola-extra-openwrt.sh
@@ -29,6 +29,8 @@ sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 sed -i 's|root:x:0:0:root:/root:/bin/ash|root:x:0:0:root:/root:/bin/bash|g' package/base-files/files/etc/passwd
 # 增加net6
 sed -i 's/odhcp6c/& net6/' include/target.mk
+# 增加smartdns
+sed -i 's/base-files/& smartdns luci-app-smartdns smartdns_config/' include/target.mk
 # 删除wifi禁用
 sed -i '/set wireless.radio${devidx}.disabled/d' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 增加 SSID 2.5G
