ce3479ee1c7c9197107e38cd549d69111ab4ed98
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-30 00:57:07 +0800

    Create uola-packages-openwrt.sh

---
+++ scripts/uola-packages-openwrt.sh
@@ -0,0 +1,13 @@
+svn co https://github.com/danxiaonuo/uola/trunk/package/xiaonuo xiaonuo
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
+svn co https://github.com/danxiaonuo/AutoSync/trunk/server/net6 xiaonuo/net6
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
+svn co https://github.com/danxiaonuo/AutoSync/trunk/server/smartdns_config xiaonuo/smartdns_config
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
+svn co https://github.com/danxiaonuo/AutoSync/trunk/server/smartdns_config xiaonuo/smartdns_config
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
+git clone https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
+git clone https://github.com/jerrykuku/luci-theme-argon.git xiaonuo/luci-theme-argon
+find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
+find /. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec ls {} \; >& /dev/null
