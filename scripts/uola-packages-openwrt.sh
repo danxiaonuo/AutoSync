2aef376969e7fa3bc8f9bcd5926e1d81fccf0c3f
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-30 01:38:41 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -1,13 +1,13 @@
 svn co https://github.com/danxiaonuo/uola/trunk/package/xiaonuo xiaonuo
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf  {} \; >& /dev/null
+find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/net6 xiaonuo/net6
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf {} \; >& /dev/null
+find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/smartdns_config xiaonuo/smartdns_config
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf {} \; >& /dev/null
+find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/smartdns_config xiaonuo/smartdns_config
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf {} \; >& /dev/null
+find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
 rm -rf xiaonuo/default-settings && rm -rf xiaonuo/luci-theme-argon
 git clone -b master https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf {} \; >& /dev/null
+find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
 git clone -b  master https://github.com/jerrykuku/luci-theme-argon.git xiaonuo/luci-theme-argon
-find xiaonuo/. -type d -regextype posix-extended -regex '.*.(svn|git)' -exec rm -rf {} \; >& /dev/null
+find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
