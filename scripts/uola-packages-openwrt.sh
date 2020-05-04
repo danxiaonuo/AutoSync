7a38bec215cea94f466b1adebffaef4a295397a5
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-05-05 00:57:14 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -10,11 +10,11 @@ git clone https://github.com/jerrykuku/luci-theme-argon xiaonuo/luci-theme-argon
 find xiaonuo/. -type d -iname '.git' | xargs rm -rf
 find . -type d -iname '.svn' | xargs rm -rf
 find . -type d -iname '.git' | xargs rm -rf
-svn co https://github.com/danxiaonuo/AutoSync/trunk/server/rpcd xiaonuo/rpcd
+svn co https://github.com/danxiaonuo/AutoSync/trunk/server/acl xiaonuo/acl
 find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
 # 修改automount配置文件
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/automount/files/15-automount > xiaonuo/automount/files/15-automount
 # 修改smartdns配置文件
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/smartdns.conf > xiaonuo/smartdns/conf/smartdns.conf
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/custom.conf > xiaonuo/smartdns/conf/custom.conf
-curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/rpcd/acl.d/luci-app-smartdns.json > xiaonuo/luci-app-smartdns/root/usr/share/rpcd/acl.d/luci-app-smartdns.json
+curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/acl/files/luci-app-smartdns.json > xiaonuo/luci-app-smartdns/root/usr/share/rpcd/acl.d/luci-app-smartdns.json
