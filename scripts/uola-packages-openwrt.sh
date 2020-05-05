fc28b6ab5e583e51c8eb90cf5ee5e89fc0e14788
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-05-05 23:20:46 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -2,7 +2,6 @@ svn co https://github.com/danxiaonuo/uola/trunk/package/xiaonuo xiaonuo
 find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
 rm -rf xiaonuo/default-settings
 rm -rf xiaonuo/luci-theme-argon
-rm -rf xiaonuo/acl
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/net6 xiaonuo/net6
 find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
 git clone https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
