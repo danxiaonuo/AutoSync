2bcd39d781ac86a14aa9851a2513856b9776fc81
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-05-30 18:55:00 +0800

    Update uola-extra-openwrt.sh

--- scripts/uola-extra-openwrt.sh
+++ scripts/uola-extra-openwrt.sh
@@ -59,3 +59,4 @@ curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/a
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/smartdns.conf > package/xiaonuo/smartdns/conf/smartdns.conf
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/custom.conf > package/xiaonuo/smartdns/conf/custom.conf
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/acld/files/luci-app-smartdns.json > package/xiaonuo/luci-app-smartdns/root/usr/share/rpcd/acl.d/luci-app-smartdns.json
+curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/01-smartdns > package/base-files/files/etc/hotplug.d/net/01-smartdns
