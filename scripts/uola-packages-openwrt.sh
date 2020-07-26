5cd11e489dcaabfc311950074d3577f148711926
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-07-26 20:27:08 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -15,7 +15,8 @@ find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
 # 修改automount配置文件
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/automount/files/15-automount > xiaonuo/automount/files/15-automount
 # 增加 smartdns
-cp -rfp net/smartdns xiaonuo/smartdns 
+cp -rfp net/smartdns xiaonuo/smartdns
+rm -rf net/smartdns
 git clone https://github.com/pymumu/luci-app-smartdns xiaonuo/luci-app-smartdns
 # 修改smartdns配置文件
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/smartdns.conf > xiaonuo/smartdns/conf/smartdns.conf
