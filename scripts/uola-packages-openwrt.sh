31785134edf03ccc6fbededed94bcf9bf8c3c7ff
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-08-14 21:08:11 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -1,22 +1,3 @@
-svn co https://github.com/danxiaonuo/uola/trunk/package/xiaonuo xiaonuo
-find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
-rm -rf xiaonuo/default-settings
-rm -rf xiaonuo/luci-theme-argon
-svn co https://github.com/danxiaonuo/AutoSync/trunk/server/net6 xiaonuo/net6
-find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
-git clone https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
-find xiaonuo/. -type d -iname '.git' | xargs rm -rf
-git clone https://github.com/jerrykuku/luci-theme-argon xiaonuo/luci-theme-argon
-find xiaonuo/. -type d -iname '.git' | xargs rm -rf
-find . -type d -iname '.svn' | xargs rm -rf
-find . -type d -iname '.git' | xargs rm -rf
-svn co https://github.com/danxiaonuo/AutoSync/trunk/server/acld xiaonuo/acld
-find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
-# 修改automount配置文件
-curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/automount/files/15-automount > xiaonuo/automount/files/15-automount
-# 增加 smartdns
-git clone https://github.com/pymumu/luci-app-smartdns xiaonuo/luci-app-smartdns
-find . -type d -iname '.git' | xargs rm -rf
 # 修改smartdns配置文件
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/smartdns.conf > net/smartdns/conf/smartdns.conf
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/custom.conf > net/smartdns/conf/custom.conf
