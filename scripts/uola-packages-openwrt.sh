bc00815af3c41b2fa6535ac8f616785a16b0571e
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-08-15 21:15:26 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -1,8 +1,11 @@
 # 增加类库
 mkdir -pv xiaonuo
-svn co --force https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package xiaonuo-dev && svn up --force
+git clone -b openwrt-19.07 --depth=1 https://github.com/project-openwrt/openwrt xiaonuo-test
+mv xiaonuo-test/package xiaonuo-dev
+rm -rf xiaonuo-test
 \cp -rpf xiaonuo-dev/ctcgfw/* xiaonuo/ && \cp -rpf xiaonuo-dev/lean/* xiaonuo/ && \cp -rpf xiaonuo-dev/lienol/* xiaonuo/ && \cp -rpf xiaonuo-dev/ntlf9t/* xiaonuo/ && \cp -rpf xiaonuo-dev/zxlhhyccc/* xiaonuo/
 rm -rf xiaonuo-dev
+find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
 # 增加net6
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/net6 xiaonuo/net6
 find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
