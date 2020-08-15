85e2079dbc58db57fd8cd171bddc18e0abe4452f
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-08-16 07:31:28 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -5,7 +5,15 @@ mv xiaonuo-test/package xiaonuo-dev
 rm -rf xiaonuo-test
 \cp -rpf xiaonuo-dev/ctcgfw/* xiaonuo/ && \cp -rpf xiaonuo-dev/lean/* xiaonuo/ && \cp -rpf xiaonuo-dev/lienol/* xiaonuo/ && \cp -rpf xiaonuo-dev/ntlf9t/* xiaonuo/ && \cp -rpf xiaonuo-dev/zxlhhyccc/* xiaonuo/
 rm -rf xiaonuo-dev
+rm -rf xiaonuo/default-settings
+rm -rf xiaonuo/luci-theme-argon*
 find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
+# 增加 default-settings
+git clone https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
+find xiaonuo/. -type d -iname '.git' | xargs rm -rf
+# 增加 luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git xiaonuo/luci-theme-argon
+find xiaonuo/. -type d -iname '.git' | xargs rm -rf
 # 增加net6
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/net6 xiaonuo/net6
 find xiaonuo/. -type d -iname '.svn' | xargs rm -rf
