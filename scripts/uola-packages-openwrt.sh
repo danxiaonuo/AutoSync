360e99fbd84a0d92d03e6005a37cc7dde83776d6
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-09-07 23:22:26 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -7,29 +7,22 @@ rm -rf xiaonuo-test
 rm -rf xiaonuo-dev
 # 增加 default-settings
 rm -rf xiaonuo/default-settings
-git clone https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
+git clone --depth=1 https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
 # 增加 luci-theme-argon
 rm -rf xiaonuo/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git xiaonuo/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git xiaonuo/luci-theme-argon
 # 增加net6
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/net6 xiaonuo/net6
 # 增加acld权限
 svn co https://github.com/danxiaonuo/AutoSync/trunk/server/acld xiaonuo/acld
 # 增加 Project OpenWrt's autocore
-pushd xiaonuo
-rm -rf autocore
-svn co https://github.com/project-openwrt/openwrt/branches/openwrt-18.06-k5.4/package/lean/autocore
-popd
+rm -rf  xiaonuo/autocore
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-18.06-k5.4/package/lean/autocore xiaonuo/autocore
 # 修复 libssh
-pushd libs
-rm -rf libssh
-svn co https://github.com/openwrt/packages/trunk/libs/libssh
-popd
+rm -rf libs/libssh
+svn co https://github.com/openwrt/packages/trunk/libs/libssh libs/libssh
 # 修复 mt76 wireless driver
-pushd kernel/mt76
-rm -f Makefile
-wget https://raw.githubusercontent.com/openwrt/openwrt/master/package/kernel/mt76/Makefile
-popd
+curl -fsSL https://raw.githubusercontent.com/openwrt/openwrt/master/package/kernel/mt76/Makefile > kernel/mt76/Makefile
 # 修改smartdns配置文件
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/smartdns.conf > net/smartdns/conf/smartdns.conf
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/custom.conf > net/smartdns/conf/custom.conf
