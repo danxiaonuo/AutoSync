4fd283249da4f1fd5bc63cc35a3c526307716b61
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-09-09 06:57:12 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -5,6 +5,8 @@ mv xiaonuo-test/package xiaonuo-dev
 rm -rf xiaonuo-test
 \cp -rpf xiaonuo-dev/ctcgfw/* xiaonuo/ && \cp -rpf xiaonuo-dev/lean/* xiaonuo/ && \cp -rpf xiaonuo-dev/lienol/* xiaonuo/ && \cp -rpf xiaonuo-dev/ntlf9t/* xiaonuo/ && \cp -rpf xiaonuo-dev/zxlhhyccc/* xiaonuo/
 rm -rf xiaonuo-dev
+# 移除设备驱动
+rm -rf xiaonuo/r8168 xiaonuo/rtl8192du
 # 增加 default-settings
 rm -rf xiaonuo/default-settings
 git clone --depth=1 https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
