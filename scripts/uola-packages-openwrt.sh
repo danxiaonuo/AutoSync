644d2abca591a11c4e123845adc49abb47625ed8
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-09-09 18:40:27 +0800

    Update uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++ scripts/uola-packages-openwrt.sh
@@ -1,12 +1,14 @@
 # 增加类库
 mkdir -pv xiaonuo
-git clone -b openwrt-18.06 https://github.com/project-openwrt/openwrt xiaonuo-test
+git clone --depth=1 -b openwrt-18.06-k5.4 https://github.com/project-openwrt/openwrt xiaonuo-test
 mv xiaonuo-test/package xiaonuo-dev
 rm -rf xiaonuo-test
 \cp -rpf xiaonuo-dev/ctcgfw/* xiaonuo/ && \cp -rpf xiaonuo-dev/lean/* xiaonuo/ && \cp -rpf xiaonuo-dev/lienol/* xiaonuo/ && \cp -rpf xiaonuo-dev/ntlf9t/* xiaonuo/ && \cp -rpf xiaonuo-dev/zxlhhyccc/* xiaonuo/
 rm -rf xiaonuo-dev
 # 移除设备驱动
 rm -rf xiaonuo/r8125 xiaonuo/r8168 xiaonuo/rtl8192du xiaonuo/rtl8821cu xiaonuo/rtl88x2bu
+# 重新命名
+mv xiaonuo/openwrt-chinadns-ng xiaonuo/chinadns-ng && mv xiaonuo/openwrt-simple-obfs xiaonuo/simple-obfs && mv xiaonuo/openwrt-ssocks xiaonuo/ssocks && mv xiaonuo/openwrt-udpspeeder xiaonuo/udpspeeder && mv xiaonuo/openwrt-v2ray-plugin xiaonuo/v2ray-plugin
 # 增加 default-settings
 rm -rf xiaonuo/default-settings
 git clone --depth=1 https://github.com/danxiaonuo/default-settings xiaonuo/default-settings
