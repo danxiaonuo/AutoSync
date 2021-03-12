30d3a8fd6e6e6de439b45c2e8a30e718041e8231
danxiaonuo <danxiaonuo@danxiaonuo.me>
2021-03-12 22:56:36 +0800

    Delete uola-packages-openwrt.sh

--- scripts/uola-packages-openwrt.sh
+++
@@ -1,3 +0,0 @@
-# 修改smartdns配置文件
-# curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/smartdns.conf > net/smartdns/conf/smartdns.conf
-# curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoSync/master/server/smartdns_config/files/custom.conf > net/smartdns/conf/custom.conf
